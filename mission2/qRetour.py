42df14db3fdf3a7e45033e272879fe07d114ee7c
thetheos <vandendriesschetheo@hotmail.com>
2018-10-05 11:28:09 +0200

    Add mission retour

---
+++ mission2/qRetour.py
@@ -0,0 +1,10 @@
+#Code réalisé le 4/10/2018 par Théo Vanden Driessche
+#Ce code print le nombre de diviseur propre d'un nombre compris entre 0 et n
+n = int(input("Rentrer une valeur: "))
+for i in  range(1,n+1):
+    compteur = 0
+    for o in range(1,i):
+        if i % o == 0 :
+            compteur += 1
+    print(i, "\t", compteur)
+    
