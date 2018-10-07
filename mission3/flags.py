a84248d4c5b436c8aa054d056b24a25e4a2b5357
thetheos <vandendriesschetheo@hotmail.com>
2018-10-07 22:54:19 +0200

    add new flag and proper commentary

--- mission3/flags.py
+++ mission3/flags.py
@@ -1,12 +1,12 @@
 import turtle                # module des graphiques tortue
+
+
 tortue = turtle.Turtle()     # créer une nouvelle tortue
 tortue.speed(2)      # tracé rapide
 wn = turtle.Screen()
 
-
 def square(size, color):
     """Trace un carré plein de taille `size` et de couleur `color`.
-
     pre: `color` spécifie une couleur.
          La tortue `tortue` est initialisée.
          La tortue est placée à un sommet et orientée en direction d'un
@@ -23,50 +23,42 @@ def square(size, color):
     tortue.end_fill()
     tortue.penup()
 
-def rectangle(widht, height, color):
-    """Trace un rectancle de larheur 'wifth' et de hauteur 'height' et de couleur 'color'
 
-    pre: color spécifie la couleur du rectangle
-        widht: largeur, height:hauteur
-        La tortue est positionnée à un sommet du rectagle
-        la tortue pointe vers le coté de la hauteur du rectangle
-    post: le rectangle a été tracé sur la droite du premier coté
-        La tortue est de retour à sa position et orientation d'origine 
+def rectangle(widht, height, color):
+    """Trace un rectancle de largeur 'wifth' et de hauteur 'height' et de couleur 'color' .
+    pre: 
+        `color` spécifie la couleur du rectangle.
+        `widht`: largeur, `height`: hauteur .
+        La tortue est positionnée à un sommet du rectagle et orientée en direction d'un coté 
+        du rectangle.
+    post:
+        Le rectangle a été tracé sur la gauche du premier coté.
+        La tortue est de retour à sa position et orientation d'origine.
     """
     tortue.color(color)
     tortue.pendown()
     tortue.begin_fill()
     for i in range(2):
         tortue.forward(widht)
-        tortue.right(90)
+        tortue.left(90)
         tortue.forward(height)
-        tortue.right(90)
+        tortue.left(90)
     tortue.end_fill()
     tortue.penup()
 
-def belgian_flag(width):
-    """Trace le drapeau belge de rapport 3/2 et de largeur width
-    pre: width largeur du drapeau
-        La tortue est positionnée à un sommet du drapeau
-        la tortue pointe vers le coté de la hauteur du drapeau
-    post: Le drapeau a été déssine à la droite du premier coté
-        La tortue est de retour en position et orientation d'origine
-    """
-    for color in ["black","yellow","red"]:
-        rectangle(width/3, (2*width)/3, color)
-        tortue.forward(width/3)
-    
-    tortue.right(180)
-    tortue.forward(width)
-    tortue.right(180)
 
 def three_color_flag(width, color1, color2, color3):
-    """Trace le drapeau de rapport 3/2 et de largeur width avec les couleurs definies
-    pre: width largeur du drapeau
-        La tortue est positionnée à un sommet du drapeau
-        la tortue pointe vers le coté de la hauteur du drapeau
-    post: Les 3 parties du drapeau ont été déssinées à la droite du premier coté
-        La tortue est de retour en position et orientation d'origine
+    """Trace un drapeau tricolore `color1`,`color2`,`color3`. De largeur `width`, 
+    le rapport L/H = 3/2.
+    pre: 
+        `widht`: largeur du drapeau.
+        `color1`,`color2`,`color3`: les couleurs du drapeau de gauche à droite.
+        La tortue est positionnée à un sommet du drapeau (Le coin inférieur gauche).
+        la tortue pointe vers le coté de la largeur du drapeau.
+    post: 
+        Le drapeau tricolore est dessiné dans le quart supérier droit par rapport à la tortue.
+        La tortue est de retour en position et orientation d'origine dans le coin inferieur gauche 
+        du drapeau.
     """
     for color in [color1,color2,color3]:
         rectangle(width/3, (2*width)/3, color)
@@ -76,21 +68,29 @@ def three_color_flag(width, color1, color2, color3):
     tortue.forward(width)
     tortue.right(180)
 
+
 def hv_three_color_flag(width, color1, color2, color3, disposition):
-    """Trace un drapeau tricolore de rapport 3/2 et de largeur width avec les couleurs definies 
-    horizontalement ou veticalemnt 
-    pre: width largeur du drapeau
-        disposition: définit l'orientation des parties du drapeau "h":horizontal ou "v": vertical
-        Les couleurs doivent être données de gauche à droite ou de bas en haut.
-        La tortue est positionnée à un sommet du drapeau
-        la tortue pointe vers le coté de la hauteur du drapeau
-    post: Les 3 parties du drapeau ont été déssinées à la droite du premier coté
-        La tortue est de retour en position et orientation d'origine
+    """Trace un drapeau tricolore `color1`,`color2`,`color3` horizontalement ou verticalement `disposition`. De largeur `width`, 
+    le rapport L/H = 3/2.
+    pre: 
+        `widht`: largeur du drapeau.
+        `color1`,`color2`,`color3`: les couleurs du drapeau de gauche à droite ou de bas en haut.
+        `disposition`: définit l'orientation des couleurs du drapeau "h":horizontal ou "v": vertical.
+        La tortue est positionnée à un sommet du drapeau (le coin inférieur droit).
+        la tortue pointe vers le coté de la largeur du drapeau.
+    post: 
+        Le drapeau tricolore est dessiné dans le quart supérier droit par rapport à la tortue
+        La tortue est de retour en position et orientation d'origine dans le coin inferieur gauche 
+        du drapeau
     """
     if disposition == "v":
         for color in [color1, color2, color3]:
             rectangle(width/3, (2*width)/3, color)
             tortue.forward(width/3)
+
+        tortue.right(180)       #Repositionne la tortue à son point d'origine de création du drapeau
+        tortue.forward(width)
+        tortue.right(180)
     elif disposition == "h":
         for color in [color1, color2, color3]:
             rectangle(width, (2*width)/9, color)
@@ -98,13 +98,79 @@ def hv_three_color_flag(width, color1, color2, color3, disposition):
             tortue.forward((2*width)/9)
             tortue.right(90)
 
-    #tortue.right(180)
-    #tortue.forward(width)
-    #tortue.right(180)
+        tortue.right(90)       #Repositionne la tortue à son point d'origine de création du drapeau
+        tortue.forward((2*width)/3)
+        tortue.left(90)
+
+
+def belgian_flag(width):
+    """Trace le drapeau belge de largeur width. Le rapport L/H = 3/2.
+    pre: 
+        `widht`: largeur du drapeau.
+        La tortue est positionnée à un sommet du drapeau
+        la tortue pointe vers le coté de la largeur du drapeau
+    post:
+        Le drapeau  est dessiné dans le quart supérier droit par rapport à la tortue
+        La tortue est de retour en position et orientation d'origine dans le coin inferieur gauche 
+        du drapeau
+    """
+    for color in ["black","yellow","red"]:
+        rectangle(width/3, (2*width)/3, color)
+        tortue.forward(width/3)
+    
+    tortue.right(180)
+    tortue.forward(width)
+    tortue.right(180)
+
+
+def dutch_flag(width):
+    """Trace le drapeau des Pays-Bas de largeur width. Le rapport L/H = 3/2.
+    pre: 
+        `widht`: largeur du drapeau.
+        La tortue est positionnée à un sommet du drapeau
+        la tortue pointe vers le coté de la largeur du drapeau
+    post:
+        Le drapeau  est dessiné dans le quart supérier droit par rapport à la tortue
+        La tortue est de retour en position et orientation d'origine dans le coin inferieur gauche 
+        du drapeau
+    """
+    hv_three_color_flag(width,"blue","white","red","h")
+
+
+def new_belgian_flag(width):
+    """Trace le drapeau belge de largeur width. Le rapport L/H = 3/2.
+    pre: 
+        `widht`: largeur du drapeau.
+        La tortue est positionnée à un sommet du drapeau
+        la tortue pointe vers le coté de la largeur du drapeau
+    post:
+        Le drapeau  est dessiné dans le quart supérier droit par rapport à la tortue
+        La tortue est de retour en position et orientation d'origine dans le coin inferieur gauche 
+        du drapeau
+    """
+    hv_three_color_flag(width,"black","yellow","red","v")
+
+
+def german_flag(width):
+    """Trace le drapeau allemand de largeur width. Le rapport L/H = 3/2.
+    pre: 
+        `widht`: largeur du drapeau.
+        La tortue est positionnée à un sommet du drapeau
+        la tortue pointe vers le coté de la largeur du drapeau
+    post:
+        Le drapeau  est dessiné dans le quart supérier droit par rapport à la tortue
+        La tortue est de retour en position et orientation d'origine dans le coin inferieur gauche 
+        du drapeau
+    """
+    hv_three_color_flag(width,"yellow","red","black","h")
+
 
 #rectangle(300,100,"green")
 #belgian_flag(300)
-three_color_flag(300,"black","yellow","red")
-hv_three_color_flag(300,"black","yellow","red","h")
+#three_color_flag(300,"black","yellow","red")
+#hv_three_color_flag(300,"black","yellow","red","h")
+dutch_flag(300)
+new_belgian_flag(300)
+german_flag(300)
 
 wn.mainloop()
